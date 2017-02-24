28179265259977efd7317fbea49de73f7746baac
Didenko Andre <didenko.andre@gmail.com>
2017-02-24 03:12:11 +0300

    we want to be perfect

--- bot.py
+++ bot.py
@@ -22,11 +22,20 @@ def send_file(bot, filename, chat_id, _type, caption, **kwargs):
     return upload_from_disk()
 
 def start(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text = "Я бот, вижу, вы хотите поботать ГОС?")
+    bot.sendMessage(chat_id=update.message.chat_id, text = "Я бот, вижу, вы хотите поботать ГОС?\n\
+	Для ознакомления со списком возможных команд, запросите /help")
+	
+def help(bot, update):
+	bot.sendMessage(chat_id=update.message.chat_id, text = "Список команд данного бота:\n\
+	/book -- получить последнюю версию ГОСБука в pdf-формате\n\
+	/subscribe -- подписаться на рассылку об обновлениях ГОСБука, чтобы автоматически получать новые версии ГОСБука, а также читать новости, касающиеся ГОСа\n\
+	/unsubscribe -- отписаться от выше указанной новостной рассылки\n\
+	/help -- вывести список команд данного бота\n\n\
+	PS. Если я как-то неправильно работаю или у тебя есть интересные предложения по улучшению меня, то напиши, пожалуйста, моему создателю @didenko_andre")
 
 def getbook(bot, update):
     send_file(bot, "/home/ec2-user/GOS_book/_main.pdf", update.message.chat_id, None,
-                      caption="Вот ваша книга")
+                      caption="Вот последняя версия ГОСбука")
 
 def get_subscribers():
     with open('subscribers.txt', 'r') as db:
@@ -55,7 +64,7 @@ def subscribe(bot, update):
         bot.sendMessage(chat_id=id, text = "Вы уже являетесь подписчиком!")
         return None
     add_subscriber(id)	
-    bot.sendMessage(chat_id=id, text = "Вы успешно подписались на рассылку о обновлениях ГОСбука!")
+    bot.sendMessage(chat_id=id, text = "Вы успешно подписались на рассылку об обновлениях ГОСбука!")
 
 def unsubscribe(bot, update):
     id = update.message.chat_id
@@ -63,7 +72,7 @@ def unsubscribe(bot, update):
         bot.sendMessage(chat_id=id, text = "Вы не являетесь подписчиком!")
         return None
     del_subscriber(id)
-    bot.sendMessage(chat_id=id, text = "Вы прекратили свою подписку о обновлениях ГОСбука!")
+    bot.sendMessage(chat_id=id, text = "Вы прекратили свою подписку на рассылку об обновлениях ГОСбука!")
 
 if __name__ == '__main__':
 	TOKEN = '305103696:AAGtt_a0EjkvU7F9ySpi1Snn6eHkMgWRW0U'
@@ -75,6 +84,8 @@ if __name__ == '__main__':
 	# Command handlers
 	start_handler = CommandHandler('start', start)
 	dispatcher.add_handler(start_handler)
+	help_handler = CommandHandler('help', help)
+	dispatcher.add_handler(help_handler)
 	book_handler = CommandHandler('book', getbook)
 	dispatcher.add_handler(book_handler)
 	subscribe_handler = CommandHandler('subscribe',subscribe)
